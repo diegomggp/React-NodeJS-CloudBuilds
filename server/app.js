8edada3776d73b8db2b8aca3de7998c380b8986e
diego <diegomg456@gmail.com>
2023-12-11 19:02:26 +0100

    prueba

--- server/app.js
+++ server/app.js
@@ -3,36 +3,67 @@ require('./utils/db_mongo')
 const express = require('express');
 const morgan = require('morgan');
 const error404 = require('./middlewares/error404')
-const app = express()
-const port = 3000
+const app = express();
 const cors = require('cors');
 
 // Módulos de Rutas
 const usersApiRoutes = require('./routes/usersApiRoutes');
 
-
 // Middlewares
 app.use(express.json()); // Habilitar tipo de dato a recibir
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(morgan('combined'))
 app.use(cors());
 
-//Rutas 
-
-app.get('/',(req,res)=>{
-    res.json({msj: `Bienvenido. Usa las siguientes rutas para obtener información sobre usuarios: http://localhost:${port}/api/users`})
-})
+// Rutas 
+app.get('/', (req, res) => {
+    const baseUrl = process.env.BASE_URL || 'http://localhost:3000'; // Puedes configurar BASE_URL como una variable de entorno en tu plataforma de alojamiento
+    res.json({ msj: `Bienvenido. Usa las siguientes rutas para obtener información sobre usuarios: ${baseUrl}/api/users` });
+});
 
 app.use('/api/users', usersApiRoutes); // Rutas web users
 
 app.use(error404); // Middleware Para ruta no encontrada (404)
 
-
-
-
+const port = process.env.PORT || 3000; // Usar el puerto proporcionado por Cloud Run o 3000 como valor predeterminado
 
 app.listen(port, () => {
-    console.log(` listening on port http://localhost:${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
+
+
+
+
+// require('dotenv').config();
+// require('./utils/db_mongo')
+// const express = require('express');
+// const morgan = require('morgan');
+// const error404 = require('./middlewares/error404')
+// const app = express();
+// const cors = require('cors');
+
+// // Módulos de Rutas
+// const usersApiRoutes = require('./routes/usersApiRoutes');
+
+// // Middlewares
+// app.use(express.json()); // Habilitar tipo de dato a recibir
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(morgan('combined'))
+// app.use(cors());
+
+// // Rutas 
+// app.get('/', (req, res) => {
+//     res.json({ msj: `Bienvenido. Usa las siguientes rutas para obtener información sobre usuarios: /api/users` })
+// });
+
+// app.use('/api/users', usersApiRoutes); // Rutas web users
+
+// app.use(error404); // Middleware Para ruta no encontrada (404)
+
+// const port = process.env.PORT || 3000; // Usar el puerto proporcionado por Cloud Run o 3000 como valor predeterminado
+
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+// });
--- server/utils/db_mongo.js
+++ server/utils/db_mongo.js
@@ -2,13 +2,19 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose.set('strictQuery', true);
 
+// Obtener la URL de conexión desde la variable de entorno
 const url = process.env.DB_URL_ATLAS;
+
+if (!url) {
+  console.error('La variable de entorno DB_URL_ATLAS no está configurada.');
+  process.exit(1);
+}
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 
 // Eventos
 db.on("error", error => console.log(error));
-db.once("open", () => console.log("connection to Mongodb established :)"));
+db.once("open", () => console.log("Connection to MongoDB established :)"));
 
 module.exports = mongoose;
